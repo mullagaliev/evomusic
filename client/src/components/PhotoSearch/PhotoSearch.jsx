cc98f533676b1551864ce32d33bc927a8331114c
Andrew <vray1995@gmail.com>
2017-12-03 02:55:56 +0300

    add people

--- client/src/components/PhotoSearch/PhotoSearch.jsx
+++ client/src/components/PhotoSearch/PhotoSearch.jsx
@@ -3,35 +3,7 @@ import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import CoverFlow from '../vendors/CoverFlow/CoverFlow';
 import { Loader } from 'semantic-ui-react';
-const nanoid = require('nanoid');
 const styles = require('./PhotoSearch.sass');
-
-class Photo {
-  constructor(url) {
-    const id = nanoid();
-    this.id = id;
-    this.url = url;
-    this.alt = id;
-    this.is = false;
-  }
-}
-let photos = [
-  new Photo('https://images5.cosmopolitan.ru/upload/img_cache/e48/e4812886bc5abe3d92a0f738958f4414_fitted_740x700.jpg'),
-  new Photo('http://cdn.fishki.net/upload/post/2016/07/19/2016725/4-02.jpg'),
-  new Photo('http://cdn.fishki.net/upload/post/201602/25/1861638/11_3.jpg'),
-  new Photo('http://www.fresher.ru/manager_content/02-2016/kak-by-vyglyadeli-zvyozdy-gollivuda-esli-by-ix-lica-byli-simmetrichnymi/10.jpg'),
-  new Photo('http://n1s2.hsmedia.ru/50/63/ed/5063ed9f9e03400a749ebe2123e2eeb0/500x600_1_08ce9b1a8a8dfdb73821fdab1480d263@500x600_0xd42ee437_20210599701431141753.jpeg'),
-];
-
-photos = photos.concat(photos,
-    photos, photos, photos,
-    photos);
-
-const PhotoItem = () => {
-  return <div>
-    <img src="" alt=""/>
-  </div>;
-};
 
 class PhotoSearch extends Component {
   state = {
@@ -39,6 +11,7 @@ class PhotoSearch extends Component {
   };
 
   render() {
+    const { photos } = this.props;
     return (
         <div className={styles.PhotoSearch}>
           <div className={styles.PhotoSearchSource}>
@@ -77,6 +50,8 @@ class PhotoSearch extends Component {
 }
 
 PhotoSearch.propTypes = {};
-PhotoSearch.defaultProps = {};
+PhotoSearch.defaultProps = {
+  photos: []
+};
 
 export default PhotoSearch;
--- client/src/containers/PhotoSearch/PhotoSearchContainer.jsx
+++ client/src/containers/PhotoSearch/PhotoSearchContainer.jsx
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PhotoSearch from '../../components/PhotoSearch/PhotoSearch';
+import people1 from '../../static/people/andr1.jpg';
+import people2 from '../../static/people/Алексей Катцын.jpg';
+import people3 from '../../static/people/Алексей Михайлов.jpg';
+import people4 from '../../static/people/Анастасия Козлова.jpg';
+import people5 from '../../static/people/Виктория Соломахина.jpg';
+import people6 from '../../static/people/Денис Санин.jpg';
+import people7 from '../../static/people/Дмитрий Анохин.jpg';
+import people8 from '../../static/people/Дмитрий Бондарев.jpg';
+import people9 from '../../static/people/Евгений Нечаев.jpg';
+import people10 from '../../static/people/Елена Киреева.jpg';
+import people11 from '../../static/people/Ирина Гофман.jpg';
+import people12 from '../../static/people/Максим Белозеров.jpg';
+import people13 from '../../static/people/Ольга Касьянова.jpg';
+import people14 from '../../static/people/Слава Паринов.jpg';
+import people15 from '../../static/people/Юля Яковлева.jpg';
+
+
+const nanoid = require('nanoid');
+
+
+class Photo {
+  constructor(url) {
+    const id = nanoid();
+    this.id = id;
+    this.url = url;
+    this.alt = id;
+    this.is = false;
+  }
+}
+let photos = [
+  new Photo(people1),
+  new Photo(people2),
+  new Photo(people3),
+  new Photo(people4),
+  new Photo(people5),
+  new Photo(people6),
+  new Photo(people7),
+  new Photo(people8),
+  new Photo(people9),
+  new Photo(people10),
+  new Photo(people11),
+  new Photo(people12),
+  new Photo(people13),
+  new Photo(people14),
+  new Photo(people15)
+];
+
+photos = photos.concat(photos,
+    photos, photos, photos,
+    photos);
 
 class PhotoSearchContainer extends Component {
   render() {
     return (
-        <PhotoSearch/>
+        <PhotoSearch photos={photos}/>
     );
   }
 }
